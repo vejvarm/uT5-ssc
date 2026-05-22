c995b6cff914524e4c30b77d6dcfadd7a3abca99
Martin Vejvar <vejvarm@gmail.com>
2026-05-22 20:29:50 +0900

    feat: added support confix and scripts for c4 runs

---
+++ .scripts/run_ssc_ut5_ep19-c4-dirty-30p-pool6x.sh
@@ -0,0 +1,49 @@
+cd ~/git/uT5-ssc
+source ~/miniconda3/bin/activate ./.conda
+
+# ______ON FIRST RUN______:
+# # serve RDF4j server in docker
+# docker run --name rdf4j_server -d -p 8181:8080 eclipse/rdf4j-workbench:latest
+
+# # serve Neo4j server in docker
+# export groups=( $( id --real --groups neo4j ) )
+# docker run \
+#     --name neo4j_server \
+#     --user="$(id -u neo4j):$(id -g neo4j)" \
+#     --group-add=$groups \
+#     -p7474:7474 -p7687:7687 \
+#     -d \
+#     -v /neo4j/data:/data \
+#     -v /neo4j/logs:/logs \
+#     -v /neo4j/import:/import \
+#     -v /neo4j/plugins:/plugins \
+#     -v /neo4j/conf:/conf \
+#     --env NEO4J_PLUGINS='["apoc", "n10s"]' \
+#     --env NEO4J_AUTH=neo4j/secretserver \
+#     --env NEO4J_db_transaction_timeout=60s \
+#     --env NEO4J_db_lock_acquisition_timeout=60s \
+#     --env CLASSPATH_PREFIX=/neo4j/lib/dozerdb-plugin-5.20.0.0.jar \
+#     graphstack/dozerdb:5.20.0.0-alpha.1
+
+# # Populate databases
+# python seq2seq/serve_rdf4j_graphs.py ~/git/uT5-ssc/.cache/downloads/extracted/c702c18c8d855b7bc0a53f5b230cd5314a83d607fea4df3ad5612a557fae3dd2/Spider4SSC --split dev
+# python seq2seq/serve_neo4j_graphs.py ~/git/uT5-ssc/.cache/downloads/extracted/c702c18c8d855b7bc0a53f5b230cd5314a83d607fea4df3ad5612a557fae3dd2/Spider4SSC --split dev
+
+# ______________________
+
+# __ON SUBSEQUENT RUNS__
+docker stop rdf4j_server_ssc_test
+docker stop neo4j_server_ssc_test
+docker start rdf4j_server
+docker start neo4j_server
+
+
+python -m seq2seq.run_seq2seq configs/5_ut5-ep19-c4-dirty-30p-pool6x/cypher_compact.json
+python -m seq2seq.run_seq2seq configs/5_ut5-ep19-c4-dirty-30p-pool6x/sql_compact.json
+python -m seq2seq.run_seq2seq configs/5_ut5-ep19-c4-dirty-30p-pool6x/sparql_compact.json
+python -m seq2seq.run_seq2seq configs/5_ut5-ep19-c4-dirty-30p-pool6x/sql_norange.json
+python -m seq2seq.run_seq2seq configs/5_ut5-ep19-c4-dirty-30p-pool6x/sparql_norange.json
+python -m seq2seq.run_seq2seq configs/5_ut5-ep19-c4-dirty-30p-pool6x/cypher_norange.json
+python -m seq2seq.run_seq2seq configs/5_ut5-ep19-c4-dirty-30p-pool6x/sql_no-schema.json
+python -m seq2seq.run_seq2seq configs/5_ut5-ep19-c4-dirty-30p-pool6x/sparql_no-schema.json
+python -m seq2seq.run_seq2seq configs/5_ut5-ep19-c4-dirty-30p-pool6x/cypher_no-schema.json
